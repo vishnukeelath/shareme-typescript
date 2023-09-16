f30ac39a7c891b45106088737454922fd68c8096
Vishnuprasad K <vpk2888@gmail.com>
2023-09-16 21:07:44 +0530

    added admin sidebar, completed admin panel layout

--- src/components/AdminPanel.tsx
+++ src/components/AdminPanel.tsx
@@ -1,7 +1,12 @@
 import { User } from "@/shared/types";
 import { fetchUser } from "@/utils/fetchUser";
 import React, { useEffect, useRef, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, Route, Routes, useNavigate } from "react-router-dom";
+import AdminSidebar from "./AdminSidebar";
+import logo from "@/assets/logo.png";
+import { HiMenu } from "react-icons/hi";
+import { RiCloseCircleFill } from "react-icons/ri";
+import UserProfile from "./UserProfile";
 
 type Props = {};
 
@@ -20,7 +25,57 @@ const AdminPanel = (props: Props) => {
     }
   }, []);
 
-  return <div>AdminPanel</div>;
+  return (
+    <div className="flex bg-gray-50 md:flex-row font-montserrat flex-col h-screen transaction-height duration-75 ease-out">
+      {/* Sidebar-Desktop */}
+      <div className="hidden md:flex h-screen flex-initial">
+        <AdminSidebar user={user && user} />
+      </div>
+      {/* Sidebar-Mobile */}
+      <div className="flex md:hidden flex-row">
+        <div className="p-3 w-full flex flex-row justify-between items-center shadow-md">
+          <HiMenu
+            fontSize={35}
+            className="cursor-pointer"
+            onClick={() => setToggleSidebar(true)}
+          />
+          <Link to="/">
+            <img src={logo} alt="logo" className="w-28" />
+          </Link>
+          <Link to={`user-profile/${user?.uid}`}>
+            <img
+              src={user?.photoURL}
+              alt="logo"
+              className="w-14 rounded-full"
+            />
+          </Link>
+        </div>
+        {toggleSidebar && (
+          <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+            <div className="absolute w-full flex justify-end items-center p-2">
+              <RiCloseCircleFill
+                fontSize={30}
+                className="cursor-pointer"
+                onClick={() => setToggleSidebar(false)}
+              />
+            </div>
+            <AdminSidebar
+              user={user && user}
+              setToggleSidebar={setToggleSidebar}
+            />
+          </div>
+        )}
+      </div>
+      <div
+        className="pb-2 flex-1 h-screen overflow-y-scroll scrollbar-hide xl:scrollbar-default"
+        ref={scrollRef}
+      >
+        <Routes>
+          <Route path="/user-profile/:userId" element={<UserProfile />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default AdminPanel;
---
+++ src/components/AdminSidebar.tsx
@@ -0,0 +1,70 @@
+import { User } from "@/shared/types";
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import logo from "@/assets/logo.png";
+import { BiHomeAlt } from "react-icons/bi";
+import { adminNavigationConfig } from "@/utils/data";
+
+const isNotActiveStyle =
+  "flex items-center px-5 gap-3 h-[40px] text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize";
+const isActiveStyle =
+  "flex items-center px-5 gap-3 bg-primary-100 h-[40px] font-semibold border-r-2 border-primary-300 transition-all duration-200 ease-in-out capitalize";
+
+type Props = {
+  user: User | undefined;
+  setToggleSidebar?: (value: boolean) => void;
+};
+
+const AdminSidebar = ({ user, setToggleSidebar }: Props) => {
+  console.log("user details in sidebar-", user);
+  const handleCloseSidebar = () => {
+    if (setToggleSidebar) setToggleSidebar(false);
+  };
+
+  return (
+    <div className="flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-[210px] scrollbar-hide">
+      <div className="flex flex-col w-full items-center">
+        {/* Logo */}
+        <Link
+          to="/"
+          className="flex px-5 gap-2 my-6 pt-1 w-[190px] items-center"
+          onClick={handleCloseSidebar}
+        >
+          <img src={logo} alt="logo" className="w-full" />
+        </Link>
+        {/* Sidebar Items */}
+        <div className="flex flex-col gap-4">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? isActiveStyle : isNotActiveStyle
+            }
+            onClick={handleCloseSidebar}
+          >
+            <BiHomeAlt fontSize={25} />
+            Home
+          </NavLink>
+          {adminNavigationConfig.map((page) => (
+            <NavLink
+              to={`/admin-panel/${page.name}`}
+              className={({ isActive }) =>
+                isActive ? isActiveStyle : isNotActiveStyle
+              }
+              onClick={handleCloseSidebar}
+              key={page.name}
+            >
+              {/* <img
+                src={category.image}
+                className="w-8 h-8 rounded-full shadow-sm"
+                alt="category"
+              /> */}
+              {page.name}
+            </NavLink>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminSidebar;
